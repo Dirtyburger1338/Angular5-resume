f98ddf533f659021a0b4d75b4f0bb52eaceaedf2
Henrik Larsson <felvaggad@hotmail.com>
2018-01-30 11:09:01 +0100

    text edit

--- PHP API/programming-languages.php
+++ PHP API/programming-languages.php
@@ -7,19 +7,19 @@ $arr = array(
         "Id" => "1",
         "Language" => "C#",
         "Stars" => "3/5",
-        "Description" => "This is the primary language I current use in my day to day basis at work."
+        "Description" => "This is the primary language I currently use in my day to day work."
       ),
       array(
         "Id" => "2",
         "Language" => "C++",
         "Stars" => "2/5",
-        "Description" => "Most of the work from my University degree was made with this. Basics assignments, O(n) notation algorithms, data structures, and more."
+        "Description" => "Most of the work from my University degree was made in this language. Assignments, O(n) notation algorithms, data structures, and more."
       ),
       array(
         "Id" => "3",
         "Language" => "Java",
         "Stars" => "1/5",
-        "Description" => "Basic console application erly on in my University degree."
+        "Description" => "Made some basic console application early on in my University degree."
       ),
       array(
         "Id" => "4",
@@ -32,7 +32,6 @@ $arr = array(
         "Language" => "Angular 2+",
         "Stars" => "1/5",
         "Description" => "This page is built with Angular 5, but I have not build anything in production - yet.
-        \n\n\n
         However, I'm intrested in building an application with Angular 5 as a front-end framework and with C# as \"Code behind\". Then with whatever API inbetween. "
       ),
       array(
@@ -45,37 +44,37 @@ $arr = array(
         "Id" => "7",
         "Language" => "CSS",
         "Stars" => "2/5",
-        "Description" => "I'm alright, I guess."
+        "Description" => "I'm alright, I guess. Try this page out on different browsers and devices for instance."
       ),
       array(
         "Id" => "8",
         "Language" => "JavaScript",
         "Stars" => "2/5",
-        "Description" => "Whatever."
+        "Description" => "var omg = \"everything is a string language\"; // *wink wink*"
       ),
       array(
         "Id" => "9",
         "Language" => "TypeScript",
         "Stars" => "1/5",
-        "Description" => "Whatever."
+        "Description" => "Compile me to pretty JS please."
       ),
       array(
         "Id" => "10",
         "Language" => "PHP",
         "Stars" => "1/5",
-        "Description" => "Whatever."
+        "Description" => "Serverside language. Used when I have an apache server e.g one.com hosting."
       ),
       array(
         "Id" => "11",
         "Language" => "PowerShell",
         "Stars" => "1/5",
-        "Description" => "Done some scripts with this."
+        "Description" => "Done some small scripts with this."
       ),
       array(
         "Id" => "12",
         "Language" => "T-SQL",
         "Stars" => "2/5",
-        "Description" => "This is the other part my day to day work."
+        "Description" => "This is the other part of my day to day work. Stored prosedures mostly."
       ),
       array(
         "Id" => "1337",
